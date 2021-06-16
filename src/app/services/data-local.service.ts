e611ee0349d122a709c4d2a42431b5b515aa40a0
Jorge.Marin <jorge_marin@live.com>
2021-06-16 00:00:45 -0500

    add method

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Registro } from '../models/registro.model';
+import { Storage } from '@ionic/storage-angular';
+import { NavController } from '@ionic/angular';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataLocalService {
   registers: Registro[] = [];
-  constructor() { }
+  constructor(private storage: Storage, private navCtrl: NavController) {
+    this.initialize();
+  }
+
+  async initialize() {
+    await this.storage.create();
+    this.registers = await this.storage.get('logs') || [];
+    console.log('inicio ', this.registers);
+  }
 
   guardarRegistro(format, text) {
     const nuevoRegistro = new Registro(format, text);
     this.registers.unshift(nuevoRegistro);
     console.log(this.registers);
+    this.save();
+  }
+
+  async save (){
+    await this.storage.set('logs', this.registers);
   }
 }
